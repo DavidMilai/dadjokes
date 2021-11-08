cc78647ee1e31ae45ece392a7ed0c801ba6da80f
DavidMilai <davidmilai67@gmail.com>
2021-11-08 19:06:13 +0300

    splash screen navigation

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dadjokes/data/database.dart';
+import 'package:dadjokes/routes.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,6 +10,22 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void initDatabase() async {
+    await db.init();
+    await Future.delayed(Duration(seconds: 1), navigate);
+  }
+
+  navigate() {
+    Navigator.popAndPushNamed(context, RouteConfig.home);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initDatabase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
