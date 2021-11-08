d6f3a0223dc78bd4fc8232d781eb2b8c38160505
DavidMilai <davidmilai67@gmail.com>
2021-11-08 18:50:51 +0300

    joke model fix and adapter generator

--- lib/data/models/joke.dart
+++ lib/data/models/joke.dart
@@ -1,10 +1,16 @@
+import 'package:hive/hive.dart';
+
+part '../adapters/joke.g.dart';
+
+@HiveType(typeId: 0)
 class Joke {
   Joke({
     this.fallback,
     this.text,
   });
-
+  @HiveField(0)
   String fallback;
+  @HiveField(1)
   String text;
 
   factory Joke.fromMap(Map<String, dynamic> json) => Joke(
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
   runApp(MyApp());
 }
 
+// flutter packages pub run build_runner build
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
