d65fbcc777381287293f44bf5f8f5bef596ce6f0
DavidMilai <davidmilai67@gmail.com>
2021-11-08 18:34:48 +0300

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:dadjokes/routes.dart';
 import 'package:flutter/material.dart';
+
+import 'config.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,20 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+      title: Config.appName,
+      theme: Config.theme,
+      initialRoute: RouteConfig.defaultRoute,
+      routes: routes,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:dadjokes/screens/splash_screen.dart';
+
+var routes = {
+  RouteConfig.defaultRoute: (context) => SplashScreen(),
+};
+
+class RouteConfig {
+  static final defaultRoute = "/";
+}
