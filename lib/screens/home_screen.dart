2fd0d529fafea64b3bf3ae19041afbd0c28617a1
DavidMilai <davidmilai67@gmail.com>
2021-11-10 20:32:00 +0300

    home screen cards

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:dadjokes/services/jokes_service.dart';
+import 'package:dadjokes/utils/color.dart';
+import 'package:dadjokes/widgets/card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:swipable_stack/swipable_stack.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -10,6 +13,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Color> cardColor = [];
+
   @override
   void initState() {
     super.initState();
@@ -26,25 +31,55 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            // Center(
-            //   child: IconButton(
-            //       onPressed: () {
-            //         jokeService.getJokes();
-            //       },
-            //       icon: Icon(Icons.add)),
-            // ),
-            // Expanded(
-            //   child: ListView.builder(
-            //       itemCount: jokeService.allJokes.length,
-            //       itemBuilder: (context, item) {
-            //         var joke = jokeService.allJokes[item];
-            //         return Text("${joke.text}*********");
-            //       }),
-            // ),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  jokeService.getJokes();
+                },
+                icon: Icon(Icons.add)),
           ],
         ),
+        body: Container(
+          color: Colors.blue,
+          child: Center(
+            child: SwipableStack(
+              stackClipBehaviour: Clip.antiAlias,
+              itemCount: jokeService.allJokes.length,
+              builder: (context, index, constraints) {
+                cardColor.add(AppColors.getColor());
+                var joke = jokeService.allJokes[index];
+                return CardDisplay(
+                  text: joke.text,
+                  cardColor: cardColor[index],
+                );
+              },
+              onSwipeCompleted: (index, direction) {
+                print('$index, $direction');
+              },
+            ),
+          ),
+        ),
+
+        // Column(
+        //   children: [
+        //     // Center(
+        //     //   child: IconButton(
+        //     //       onPressed: () {
+        //     //         jokeService.getJokes();
+        //     //       },
+        //     //       icon: Icon(Icons.add)),
+        //     // ),
+        //     // Expanded(
+        //     //   child: ListView.builder(
+        //     //       itemCount: jokeService.allJokes.length,
+        //     //       itemBuilder: (context, item) {
+        //     //         var joke = jokeService.allJokes[item];
+        //     //         return Text("${joke.text}*********");
+        //     //       }),
+        //     // ),
+        //   ],
+        // ),
       ),
     );
   }
--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1,64 +1,48 @@
-import 'package:dadjokes/data/models/shot_card.dart';
 import 'package:flutter/material.dart';
 
 class CardDisplay extends StatelessWidget {
-  final ShotCard shotCard;
+  final String text;
 
-  CardDisplay({this.shotCard});
+  CardDisplay({this.text, this.cardColor});
+  final Color cardColor;
 
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
-    final bool screenSmall = screenWidth < 370;
+    Size size = MediaQuery.of(context).size;
 
-    double _cardwidth = screenSmall ? 270.0 : 320.0;
-    double _cardHeight = _cardwidth * 1.4375;
-    // 1.4375 is the ratio of the height to the width
-
-    return Transform.translate(
-      offset: shotCard.offset ?? Offset(0, 0),
-      child: Transform.rotate(
-        angle: shotCard.rotateAngle ?? 0,
-        child: Container(
-          height: _cardHeight,
-          width: _cardwidth,
-          padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: shotCard.color,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              width: 10,
-              color: Colors.transparent.withOpacity(0.3),
-              // color: Colors.black,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.22),
-                offset: Offset(0, 0),
-                blurRadius: 2,
-                spreadRadius: 0,
-              ),
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.2),
-                offset: Offset(1, 1),
-                blurRadius: 2,
-                spreadRadius: 0,
-              )
-            ],
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              // line1 is there for all cards
-              Text(
-                shotCard.text,
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  color: Colors.white,
-                ),
-                textScaleFactor: 1.0,
-              ),
-            ],
+    return Container(
+      height: size.height * 0.7,
+      width: size.width * 0.7,
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          width: 10,
+          //color: Colors.transparent.withOpacity(0.3),
+          color: Colors.black,
+        ),
+        boxShadow: [
+          // BoxShadow(
+          //   color: Color.fromRGBO(0, 0, 0, 0.22),
+          //   offset: Offset(0, 0),
+          //   blurRadius: 2,
+          //   spreadRadius: 0,
+          // ),
+          // BoxShadow(
+          //   color: Color.fromRGBO(0, 0, 0, 0.2),
+          //   offset: Offset(1, 1),
+          //   blurRadius: 2,
+          //   spreadRadius: 0,
+          // )
+        ],
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
           ),
         ),
       ),
