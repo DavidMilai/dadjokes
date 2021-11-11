af588b26608fb2aafb554d11a605c1a6c24da681
DavidMilai <davidmilai67@gmail.com>
2021-11-11 22:02:28 +0300

    Revert "selector"
    This reverts commit 39396d16

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,11 @@
 import 'dart:math';
 
-import 'package:dadjokes/data/models/joke.dart';
 import 'package:dadjokes/services/jokes_service.dart';
 import 'package:dadjokes/utils/color.dart';
 import 'package:dadjokes/widgets/card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:swipeable_card_stack/swipe_controller.dart';
-import 'package:swipeable_card_stack/swipeable_card_stack.dart';
+import 'package:swipable_stack/swipable_stack.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -19,10 +16,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Color> cardColor = [];
-  List<int> number = [];
   Random random = new Random();
-  SwipeableCardSectionController _cardController =
-      SwipeableCardSectionController();
 
   @override
   void initState() {
@@ -40,52 +34,32 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    jokeService.getJokes();
-                  },
-                  icon: Icon(Icons.add)),
-            ],
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Selector<JokeService, List<Joke>>(
-                  selector: (context, jokeService) => jokeService.allJokes,
-                  builder: (context, jokes, _) {
-                    jokes.forEach((element) {
-                      cardColor.add(AppColors.getColor());
-                    });
-                    return SwipeableCardsSection(
-                      cardController: _cardController,
-                      context: context,
-                      items: [
-                        CardDisplay(
-                          text: jokes[0].text,
-                          cardColor: cardColor[0],
-                        ),
-                        CardDisplay(
-                          text: jokes[1].text,
-                          cardColor: cardColor[1],
-                        ),
-                        CardDisplay(
-                          text: jokes[2].text,
-                          cardColor: cardColor[2],
-                        ),
-                      ],
-                      onCardSwiped: (dir, index, widget) {
-                        jokeService.getJokes();
-                        cardColor.add(AppColors.getColor());
-                        _cardController.addItem(CardDisplay());
-                      },
-                      enableSwipeUp: true,
-                      enableSwipeDown: false,
-                    );
-                  }),
-            ],
-          )),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  jokeService.getJokes();
+                },
+                icon: Icon(Icons.add)),
+          ],
+        ),
+        body: SwipableStack(
+          stackClipBehaviour: Clip.antiAlias,
+          itemCount: jokeService.allJokes.length,
+          builder: (context, index, constraints) {
+            int randNo = random.nextInt(5) + 1;
+            cardColor.add(AppColors.getColor());
+            var joke = jokeService.allJokes[index];
+            return CardDisplay(
+              text: joke.text,
+              cardColor: cardColor[index],
+            );
+          },
+          onSwipeCompleted: (index, direction) {
+            print('$index, $direction');
+          },
+        ),
+      ),
     );
   }
 }
--- lib/services/jokes_service.dart
+++ lib/services/jokes_service.dart
@@ -29,6 +29,7 @@ class JokeService extends ChangeNotifier {
     return api.getJoke().then((response) {
       if (response.data['attachments'] != null)
         _saveJoke(response.data['attachments']);
+      print(db.jokes.length);
       isLoadingJokes = false;
       return response;
     }).catchError((error) {
@@ -38,7 +39,7 @@ class JokeService extends ChangeNotifier {
   }
 
   _saveJoke(data) async {
-    if (db.jokes.length == 20) await db.jokes.clear();
+    if (db.jokes.length == 10) await db.jokes.clear();
     db.jokes.add(Joke.fromMap(data[0]));
   }
 }
