7af31d82622fc4230962d84a7c20a5c04e6b51a2
DavidMilai <davidmilai67@gmail.com>
2021-11-17 20:03:52 +0300

    removed text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -80,7 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           final opacity = min(swipeProgress, 1.0);
                           return Opacity(
                             opacity: opacity,
-                            child: Text("Hello"),
+                            child: Text(""),
                           );
                         },
                       ),
